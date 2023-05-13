b5a3aaa2b882b6aaef74772fde978d1a2b664f92
ParadoxV5 <paradox.ver5@gmail.com>
2023-05-13 17:14:26 -0600

    8 cont.

--- 8 Arrays.c
+++ 8 Arrays.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 
+#define put() puts("")
+
 int main() {
+  int i; // `for` loop counter
+  
   // An array is actually just a pointer to a continuous section of memory
   int a[5]; // Uninitialized – possibly junk values!
   printf("Address of a[0]:\t%p\n", &a[0]);
   printf("Value of a:\t%p\n", a);
-  for(int i = 0; i < 5; ++i)
+  for(i = 0; i < 5; ++i)
     printf("%i\t", a[i]);
-  puts("");
+  put();
   
   // Initialized
   int b[/*5*/] = {1, 22, 333, 4444, 55555};
-  for(int i = 0; i < 5; ++i)
+  for(i = 0; i < 5; ++i)
     printf("%i\t", b[i]);
-  puts("");
+  put();
   // Under the hood
   int* p_end = b + 5;
   for(int* p = b; p < p_end; ++p)
     printf("%i\t", *p);
-  puts("\n");
+  put();
   // ptr[0] = *ptr; ptr[1] = *(ptr+1)
+  // (Compiler auto-adjusts increment for the pointer type of `int`)
   
   /* Can’t do
   a = {420, 69}; // reassign entire array
@@ -35,10 +40,10 @@ int main() {
   char* also_immutable = "Also Immutable"; // well the pointer *is* pointing to a char
   printf("%c\t", also_immutable[5]); //=> I
   //also_immutable[6] = 'M'; //=! segfault
-  char modifyable[] = "Modifyable"; // kinda like casted to an array of not-`const` chars
-  printf("%c\t", modifyable[5]); //=> y
-  modifyable[6] = 'A';
-  puts(modifyable);
+  char modifiable[] = "Modifiable"; // kinda like casted to an array of not-`const` chars
+  printf("%c\t", modifiable[5]); //=> i
+  modifiable[6] = 'A';
+  puts(modifiable);
   
   // 2D_array[outer][inner]
   const char strings[][3] = {
@@ -46,9 +51,20 @@ int main() {
     {'a', 'b', 'c'}, // incomplete row
     'x', 'y', 'z', '&' // inner curly braces are just for show
   };
-  for(int i = 0; i < 4; ++i) {
+  for(i = 0; i < 4; ++i) {
     printf("\t%p\t", strings[i]);
     for (int j = 0; j < 3; ++j)
       printf("%c", strings[i][j]);
   }
+  put();
+  
+  // Array × Pointer
+  int* array_of_int_pointers[] = {a, b + 2, p_end - 1};
+  for(i = 0; i < 3; ++i)
+    printf("%i\t", *array_of_int_pointers[i]);
+  put();
+  int(* pointer_to_int_array)[] = &b;
+  for(i = 0; i < 5; ++i)
+    printf("%i\t", (*pointer_to_int_array)[i]);
+  put();
 }
